5f0bacb123ea68b5a274cb43c262f159af38a673
yifeixiang <GW00303711@tech.gwm.cn>
2022-06-22 14:14:53 +0800

    修改初始时显示的app列表

--- src/renderer/store/modules/AndroidAppList.js
+++ src/renderer/store/modules/AndroidAppList.js
@@ -1,13 +1,9 @@
 import Store from 'electron-store'
 const store = new Store()
 
-const defaultList = [
-  {name: '微信', packageName: 'com.tencent.mm/com.tencent.mm.ui.LauncherUI', icon: 'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3336519328,2903738409&fm=26&gp=0.jpg', shape: 'square'},
-  {name: '小场景', packageName: 'com.tencent.wecarmas/com.tencent.wecarmas.ui.activity.HomeActivity', icon: require('@/assets/wecarmas.png'), shape: 'circle'}
-]
 
 const state = {
-  list: store.get('androidAppList') || defaultList
+  list: store.get('androidAppList') || []
 }
 
 const mutations = {
